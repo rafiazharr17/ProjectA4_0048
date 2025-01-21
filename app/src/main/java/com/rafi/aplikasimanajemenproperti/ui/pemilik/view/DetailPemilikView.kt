983a00fa0984fbeb47cacb7ab2bd8c8baf7304c9
rafiazharr17 <rafipilo17@gmail.com>
2025-01-22 00:15:06 +0700

    membuat fungsi ComponentDetailPemilik

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
@@ -1,5 +1,14 @@
 package com.rafi.aplikasimanajemenproperti.ui.pemilik.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 import com.rafi.aplikasimanajemenproperti.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetailPemilik : DestinasiNavigasi {
@@ -7,4 +16,28 @@ object DestinasiDetailPemilik : DestinasiNavigasi {
     override val titleRes = "Detail Pemilik"
     const val ID = "idPemilik"
     val routeWithArg = "$route/{$ID}"
+}
+
+@Composable
+fun ComponentDetailPemilik(
+    modifier: Modifier = Modifier,
+    judul: String,
+    isinya: String,
+){
+    Column (
+        modifier = modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = "$judul : ",
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Gray
+        )
+        Text(
+            text = isinya,
+            fontSize = 20.sp,
+            fontWeight = FontWeight.Bold
+        )
+    }
 }
