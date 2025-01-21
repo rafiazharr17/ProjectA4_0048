af9791a0808bef46083e2d32c18ee0c6f4a5aae8
rafiazharr17 <rafipilo17@gmail.com>
2025-01-22 00:17:44 +0700

    membuat fungsi ItemDetailPemilik

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
@@ -2,13 +2,19 @@ package com.rafi.aplikasimanajemenproperti.ui.pemilik.view
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.rafi.aplikasimanajemenproperti.model.Pemilik
 import com.rafi.aplikasimanajemenproperti.ui.navigation.DestinasiNavigasi
 
 object DestinasiDetailPemilik : DestinasiNavigasi {
@@ -16,6 +22,26 @@ object DestinasiDetailPemilik : DestinasiNavigasi {
     override val titleRes = "Detail Pemilik"
     const val ID = "idPemilik"
     val routeWithArg = "$route/{$ID}"
+}
+
+@Composable
+fun ItemDetailPemilik(
+    modifier: Modifier = Modifier,
+    pemilik: Pemilik
+) {
+    Card (
+        modifier = modifier.padding(16.dp),
+        shape = MaterialTheme.shapes.medium,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        Column(
+            modifier = Modifier.padding(16.dp)
+        ) {
+            ComponentDetailPemilik(judul = "ID Pemilik", isinya = pemilik.idPemilik.toString())
+            ComponentDetailPemilik(judul = "Nama Pemilik", isinya = pemilik.namaPemilik)
+            ComponentDetailPemilik(judul = "Kontak Pemilik", isinya = pemilik.kontakPemilik)
+        }
+    }
 }
 
 @Composable
