2eb62c71e96ccdacea09a273359c0097999fd225
rafiazharr17 <rafipilo17@gmail.com>
2025-01-22 00:22:18 +0700

    membuat fungsi DetailPemilikView

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/pemilik/view/DetailPemilikView.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -16,15 +18,45 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.rafi.aplikasimanajemenproperti.model.Pemilik
+import com.rafi.aplikasimanajemenproperti.ui.PenyediaViewModel
+import com.rafi.aplikasimanajemenproperti.ui.customwidget.CostumeTopAppBar
 import com.rafi.aplikasimanajemenproperti.ui.navigation.DestinasiNavigasi
 import com.rafi.aplikasimanajemenproperti.ui.pemilik.viewmodel.DetailPemilikUiState
+import com.rafi.aplikasimanajemenproperti.ui.pemilik.viewmodel.DetailPemilikViewModel
 
 object DestinasiDetailPemilik : DestinasiNavigasi {
-    override val route = "detail"
+    override val route = "detail_pemilik"
     override val titleRes = "Detail Pemilik"
     const val ID = "idPemilik"
     val routeWithArg = "$route/{$ID}"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailPemilikView(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailPemilikViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    Scaffold(
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiDetailPemilik.titleRes,
+                canNavigateBack = true,
+                canRefresh = false,
+                onRefresh = {},
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        DetailPemilikStatus(
+            modifier = Modifier.padding(innerPadding),
+            detailPemilikUiState = viewModel.pemilikDetailState,
+            retryAction = { viewModel.getPemilikbyID() }
+        )
+    }
 }
 
 @Composable
