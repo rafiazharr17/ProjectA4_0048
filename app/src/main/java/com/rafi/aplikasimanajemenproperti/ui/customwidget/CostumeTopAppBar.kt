4e468042a2eccd49a0b274b70a16db840df7a176
rafiazharr17 <rafipilo17@gmail.com>
2025-01-22 20:59:05 +0700

    menambahkan fontweight dan color di CostumeTopAppBar

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/customwidget/CostumeTopAppBar.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ui/customwidget/CostumeTopAppBar.kt
@@ -1,6 +1,9 @@
 package com.rafi.aplikasimanajemenproperti.ui.customwidget
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,9 +13,13 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -27,7 +34,7 @@ fun CostumeTopAppBar(
     onRefresh: () -> Unit = {},
 ){
     CenterAlignedTopAppBar(
-        title = { Text(title) },
+        title = { Text(text = title, fontWeight = FontWeight.Bold) },
         actions = {
             if (canRefresh){
                 Icon(
@@ -48,6 +55,8 @@ fun CostumeTopAppBar(
                     )
                 }
             }
-        }
+        },
+        colors = TopAppBarDefaults.topAppBarColors(Color.Cyan)
     )
+
 }
