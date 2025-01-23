75d916d0e744235c9450ece19d0f2204314cd11c
rafiazharr17 <rafipilo17@gmail.com>
2025-01-23 22:40:07 +0700

    menambah lateinit variabel untuk containerJenis dan menambahkannya di fungsi onCreate

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ManajemenPropertiApp.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ManajemenPropertiApp.kt
@@ -1,17 +1,21 @@
 package com.rafi.aplikasimanajemenproperti
 
 import android.app.Application
+import com.rafi.aplikasimanajemenproperti.di.AppContainerJenisProperti
 import com.rafi.aplikasimanajemenproperti.di.AppContainerManajerProperti
 import com.rafi.aplikasimanajemenproperti.di.AppContainerPemilik
+import com.rafi.aplikasimanajemenproperti.di.JenisPropertiContainer
 import com.rafi.aplikasimanajemenproperti.di.ManajerPropertiContainer
 import com.rafi.aplikasimanajemenproperti.di.PemilikContainer
 
 class ManajemenPropertiApp: Application() {
     lateinit var container: AppContainerPemilik
     lateinit var containerManajer: AppContainerManajerProperti
+    lateinit var containerJenis: AppContainerJenisProperti
     override fun onCreate() {
         super.onCreate()
         container = PemilikContainer()
         containerManajer = ManajerPropertiContainer()
+        containerJenis = JenisPropertiContainer()
     }
 }
