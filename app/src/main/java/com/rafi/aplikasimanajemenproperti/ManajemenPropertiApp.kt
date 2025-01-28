e6be9e6296e66b4ebd9e4c12d875bd1e1c76736e
rafiazharr17 <rafipilo17@gmail.com>
2025-01-28 14:22:10 +0700

    menambahkan inisialisasi containerProperti dengan instance PropertiContainer

--- app/src/main/java/com/rafi/aplikasimanajemenproperti/ManajemenPropertiApp.kt
+++ app/src/main/java/com/rafi/aplikasimanajemenproperti/ManajemenPropertiApp.kt
@@ -4,18 +4,22 @@ import android.app.Application
 import com.rafi.aplikasimanajemenproperti.di.AppContainerJenisProperti
 import com.rafi.aplikasimanajemenproperti.di.AppContainerManajerProperti
 import com.rafi.aplikasimanajemenproperti.di.AppContainerPemilik
+import com.rafi.aplikasimanajemenproperti.di.AppContainerProperti
 import com.rafi.aplikasimanajemenproperti.di.JenisPropertiContainer
 import com.rafi.aplikasimanajemenproperti.di.ManajerPropertiContainer
 import com.rafi.aplikasimanajemenproperti.di.PemilikContainer
+import com.rafi.aplikasimanajemenproperti.di.PropertiContainer
 
 class ManajemenPropertiApp: Application() {
     lateinit var container: AppContainerPemilik
     lateinit var containerManajer: AppContainerManajerProperti
     lateinit var containerJenis: AppContainerJenisProperti
+    lateinit var containerProperti: AppContainerProperti
     override fun onCreate() {
         super.onCreate()
         container = PemilikContainer()
         containerManajer = ManajerPropertiContainer()
         containerJenis = JenisPropertiContainer()
+        containerProperti = PropertiContainer()
     }
 }
